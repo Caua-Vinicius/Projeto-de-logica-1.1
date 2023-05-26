849922098c48cafb34d68b0cf3affdd32a7af920
joao vinicius <joaoviniciusp4@gmail.com>
2023-05-26 12:09:49 -0300

    Update Pikomon.java

--- Pikomon.java
+++ Pikomon.java
@@ -14,24 +14,24 @@ public class Pikomon {
     public static ArrayList<Integer> pesquisa = new ArrayList<>();
 
     /*
-      Criado os 5 primeiros arraylist para o cadastro de pokemon, e por ultimo um
-      array para ser usado de filtro
+     * Criado os 5 primeiros arraylist para o cadastro de pokemon, e por ultimo um
+     * array para ser usado de filtro
      */
 
     /*
-      No recebimento dos grupos, teremos uma lista com numeros que o cliente
-      decidira o grupo com base no numero do grupo indicado em uma lista, e no
-      codigo esse numero sera transformado em uma string e adicionada no array com
-      o nome correto do grupo
+     * No recebimento dos grupos, teremos uma lista com numeros que o cliente
+     * decidira o grupo com base no numero do grupo indicado em uma lista, e no
+     * codigo esse numero sera transformado em uma string e adicionada no array com
+     * o nome correto do grupo
      */
 
     /* Em relação, ao sexo, sera a mesma coisa acima */
 
-        /*
-         * Opções do Return
-         *  1   - Pokemon Registrado com sucesso ou deu certo
-         * -2   - Pokemon Já Registrado ou tem problema
-         */
+    /*
+     * Opções do Return
+     * 1 - Pokemon Registrado com sucesso ou deu certo
+     * -2 - Pokemon Já Registrado ou tem problema
+     */
 
     public int read_nome(String nome) {
         int tamanho_lista = nomes.size();
@@ -39,9 +39,14 @@ public class Pikomon {
         int checagem = 0;
 
         /*
-         Nessa função temos a pesquisa baseada no nome do pokemon, que traduzindo o codigo para um portugol, temos:
-         ENQUANTO a checagem for menor que o tamanho da lista, o programa vai de linha em linha procurando um nome identico ao digitado, SE o nome existir, ele ira RETORNAR o valor da função, com o indice que se encontra o nome, CASO não exista nenhum nome na lista de nomes identico ao informado, o codigo sairá do WHILE e ira retornar -2 que é o valor de erro ou de não encontrado
-        */
+         * Nessa função temos a pesquisa baseada no nome do pokemon, que traduzindo o
+         * codigo para um portugol, temos:
+         * ENQUANTO a checagem for menor que o tamanho da lista, o programa vai de linha
+         * em linha procurando um nome identico ao digitado, SE o nome existir, ele ira
+         * RETORNAR o valor da função, com o indice que se encontra o nome, CASO não
+         * exista nenhum nome na lista de nomes identico ao informado, o codigo sairá do
+         * WHILE e ira retornar -2 que é o valor de erro ou de não encontrado
+         */
 
         while (checagem <= tamanho_lista) {
             nomes_busca = nomes.get(checagem);
@@ -55,43 +60,51 @@ public class Pikomon {
         return -2;
     }
 
-    public int read_grupo(int grupo){
+    public int read_grupo(int grupo) {
         int checagem = 0;
         int grupo_busca, grupo_busca2;
         int tamanho_lista = grupos1.size();
 
         /*
-        Após receber o nome, a função ira averiguar todas a lista de Grupos tanto o 1 como o 2 ENQUANTO a checagem for menor que o tamanho da lista de pokemons, e SE o grupo informado for IGUAL a algum existente em alguns dos grupos(1 ou 2), ele irá salvar o indece que nesse caso é a checagem, no ARRAY pesquisa e vai fazendo isso até a checagem ser igual ao tamanho do array grupos1, após terminar esse looping, o programa irá averiguar se o tamanho do ARRAY de pesquisa for maior que 0, assim retornando 1 e confirmando que houve resultado para as buscas, SENAO ele ira retornar -2 indicando que não houve resultados
-         
-        */
+         * Após receber o nome, a função ira averiguar todas a lista de Grupos tanto o 1
+         * como o 2 ENQUANTO a checagem for menor que o tamanho da lista de pokemons, e
+         * SE o grupo informado for IGUAL a algum existente em alguns dos grupos(1 ou
+         * 2), ele irá salvar o indece que nesse caso é a checagem, no ARRAY pesquisa e
+         * vai fazendo isso até a checagem ser igual ao tamanho do array grupos1, após
+         * terminar esse looping, o programa irá averiguar se o tamanho do ARRAY de
+         * pesquisa for maior que 0, assim retornando 1 e confirmando que houve
+         * resultado para as buscas, SENAO ele ira retornar -2 indicando que não houve
+         * resultados
+         * 
+         */
 
-        while(checagem <= tamanho_lista){
+        while (checagem <= tamanho_lista) {
             grupo_busca = grupos1_int.get(checagem);
             grupo_busca2 = grupos2_int.get(checagem);
 
-            if(grupo == grupo_busca || grupo == grupo_busca2){
+            if (grupo == grupo_busca || grupo == grupo_busca2) {
                 pesquisa.add(checagem);
             }
         }
 
-        if(pesquisa.size()>0){
+        if (pesquisa.size() > 0) {
             return 1;
-        }
-        else{
+        } else {
             return -2;
         }
-        
+
     }
-    
 
     public int create(String nome, int grupo_int, int grupo2_int, int iv, String sexo) {
 
-        /* 
-        A checagem vai receber o valor do return da função read_nome(), dependendo do resultado ira retornar que não foi possivel realizar o cadastro porque ja existia um pokemon, ou ira adicionar normalmente 
+        /*
+         * A checagem vai receber o valor do return da função read_nome(), dependendo do
+         * resultado ira retornar que não foi possivel realizar o cadastro porque ja
+         * existia um pokemon, ou ira adicionar normalmente
          */
 
         int checagem = read_nome(nome);
-        if(checagem<0){
+        if (checagem < 0) {
             return -2;
         }
 
@@ -105,26 +118,24 @@ public class Pikomon {
         return 1;
     }
 
-    
-
     public int remove(String nome, int checagem) {
         checagem = 0;
         int tamanho_lista = nomes.size();
         String nome_busca = "";
-    /*
-      A variavel checagem sera utilizada para ser o meio de busca nos arryas, e do
-      mesmo jeito temos o tamanho da lista para ser um limite quando começar a
-      buscar o nome do pokemon, e por ultimo temos a variavel, nome_busca que é uma
-      strig vazia que irá receber o nome do array nomes de acordo com o index e
-      checagem
-      
-      Dentro do while, o index dos nomes será visto um por um até identificar um
-      nome igual, e caso tenha, vai quebrar o looping de busca e irá realizar
-     normalmente a deletagem do pokemon dentro do crud.
-      
-      Alem disso, caso a busca não de nenhum resultado, vai retornar o valor 2 que
-      sera usado para identificar que não foi possivel realizar a função
-     */
+        /*
+         * A variavel checagem sera utilizada para ser o meio de busca nos arryas, e do
+         * mesmo jeito temos o tamanho da lista para ser um limite quando começar a
+         * buscar o nome do pokemon, e por ultimo temos a variavel, nome_busca que é uma
+         * strig vazia que irá receber o nome do array nomes de acordo com o index e
+         * checagem
+         * 
+         * Dentro do while, o index dos nomes será visto um por um até identificar um
+         * nome igual, e caso tenha, vai quebrar o looping de busca e irá realizar
+         * normalmente a deletagem do pokemon dentro do crud.
+         * 
+         * Alem disso, caso a busca não de nenhum resultado, vai retornar o valor 2 que
+         * sera usado para identificar que não foi possivel realizar a função
+         */
 
         while (checagem <= tamanho_lista) {
             nome_busca = nomes.get(checagem);
@@ -167,7 +178,7 @@ public class Pikomon {
             System.out.println("2-remover");
             escolha = input.nextInt();
 
-            if(escolha == 1){
+            if (escolha == 1) {
                 System.out.println("Adicionar o nome do pokemon");
                 escolha_nome = input.next();
 
@@ -184,7 +195,7 @@ public class Pikomon {
                 System.out.println(" 10- Monster");
                 System.out.println(" 11- Water 1");
                 System.out.println(" 12- Water 2");
-                System.out.println(" 13- Water 3"); 
+                System.out.println(" 13- Water 3");
                 System.out.println("Escolha um dos grupos listados");
                 escolha_grupo1 = input.nextInt();
 
@@ -202,7 +213,7 @@ public class Pikomon {
                 System.out.println(" 10- Monster");
                 System.out.println(" 11- Water 1");
                 System.out.println(" 12- Water 2");
-                System.out.println(" 13- Water 3"); 
+                System.out.println(" 13- Water 3");
                 System.out.println("Escolha um dos grupos listados");
                 escolha_grupo2 = input.nextInt();
 
@@ -211,9 +222,8 @@ public class Pikomon {
 
                 System.out.println("Quantos ivs perfeitos tem de 0 a 6 ");
                 escolha_ivs = input.nextInt();
-                 
 
-            } 
+            }
             if (escolha == 2) {
                 System.out.println("qual o nome do pokemon quer excluir");
                 escolha_excluir = input.next();
@@ -234,7 +244,7 @@ public class Pikomon {
                 }
             }
 
-            if (escolha == 4){
+            if (escolha == 4) {
                 System.out.println("Escolha dois pokemons para testar o match");
                 System.out.println("Digite o nome do primeiro pokemon");
                 nome_match1 = input.next();
@@ -243,10 +253,10 @@ public class Pikomon {
                 nome_match2 = input.next();
 
             }
-            if(escolha == 5) {
+            if (escolha == 5) {
             }
 
-            if(escolha == 6) {
+            if (escolha == 6) {
                 break;
             }
         }
